f9dadcded28bb55b0adeb1c5c124c6d7a864b1bb
Phuc Vui <phucvui16495@gmail.com>
2020-09-16 08:29:44 +0700

    VuiTP - Add Gitignore File

---
+++ WatchShop/WatchShop/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WatchShop.Repositories;
+
+namespace WatchShop.Controllers
+{
+    public class CategoryController : Controller
+    {      
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
